6979ce07d9e76c00474a142dc0c6ebd3e43f0a25
Nguyen Duong <muoi07052001@gmail.com>
2023-10-04 22:37:28 +0700

    chore: increase token expire time

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtStrategy } from './jwt.strategy';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '86400s' }, // 1 day
     }),
   ],
   controllers: [AuthController],
