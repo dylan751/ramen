4a575507368a24b7373648451a203020fa3f485b
Nguyen Duong <muoi07052001@gmail.com>
2023-10-25 22:26:45 +0700

    chore: move datasource file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrganizationUsersModule } from './modules/organizations/users/users.module';
-import { dataSourceOptions } from './db/data-source';
 import { OrganizationsModule } from './modules/organizations/organizations.module';
 import { OrganizationRolesModule } from './modules/organizations/roles/roles.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { RouterModule } from '@nestjs/core';
 import { routes } from './route';
 import configuration from './config/configuration';
+import { dataSourceOptions } from './db/datasource/default';
 
 @Module({
   imports: [
