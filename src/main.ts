cad1f6ebd23aa11d378b8ae34d6172980f9b627a
Nguyen Duong <muoi07052001@gmail.com>
2024-05-23 00:40:38 +0700

    chore: update https

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,19 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-const corsOptions = {
-  origin: [
-    'https://hdwallet.toolhub.app',
-    'https://www.hdwallet.toolhub.app',
-    'https://cashbook.toolhub.app',
-    'https://www.cashbook.toolhub.app',
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-};
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: corsOptions });
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableCors({
+    origin: [
+      'https://hdwallet.toolhub.app',
+      'https://www.hdwallet.toolhub.app',
+      'https://cashbook.toolhub.app',
+      'https://www.cashbook.toolhub.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('GR API')
